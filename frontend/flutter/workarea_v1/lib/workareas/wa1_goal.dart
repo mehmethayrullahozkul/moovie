3cc2ff01fd584ea543e36deb6edcbe6bb333a727
mehmethayrullahozkul <hayrullahozkulb@gmail.com>
2024-04-05 13:55:22 +0300

    goal 1 changed

--- frontend/flutter/workarea_v1/lib/workareas/wa1_goal.dart
+++ frontend/flutter/workarea_v1/lib/workareas/wa1_goal.dart
@@ -36,26 +36,46 @@ class _GoalState extends State<_Goal> {
 
   @override
   Widget build(BuildContext context) {
-    int randomColorPicker = math.Random().nextInt(3);
-    mainColor = generateRandomWhitishColor(preference: randomColorPicker);
+    int randomMainColorPicker = math.Random().nextInt(3);
+    int randomSubColorPicker = math.Random().nextInt(3);
+    mainColor = generateRandomWhitishColor(
+        preference: randomMainColorPicker,
+        subPreference: randomSubColorPicker,
+        prominance: 0.5);
     return Scaffold(
       backgroundColor: mainColor,
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: (MediaQuery.of(context).size.width - 320) / 2,
-          right: (MediaQuery.of(context).size.width - 320) / 2,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {});
-                },
-                child: Text("Reload All Page")),
-            _ColorfullButton1(),
-          ],
+      body: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            setState(() {
+              counter++;
+            });
+          },
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+                padding: EdgeInsets.only(
+                  left: (MediaQuery.of(context).size.width - 320) / 2,
+                  right: (MediaQuery.of(context).size.width - 320) / 2,
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        counter.toString(),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                      _ColorfullButton1(),
+                    ],
+                  ),
+                )),
+          ),
         ),
       ),
     );
@@ -80,27 +100,91 @@ class _ColorfullButton1State extends State<_ColorfullButton1> {
 
   @override
   Widget build(BuildContext context) {
-    int randomColorPicker = math.Random().nextInt(3);
-    Color mainColor = generateRandomWhitishColor(preference: randomColorPicker);
-    return Container(
-      width: 320,
-      color: mainColor,
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-            onTap: () {
-              setState(() {
-                counter++;
-              });
-            },
-            child: Padding(
-              padding: EdgeInsets.all(50),
-              child: Text(
-                "Reload Only Me $counter",
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-            )),
-      ),
+    int randomMainColorPicker = math.Random().nextInt(3);
+    int randomSubColorPicker = math.Random().nextInt(3);
+    Color mainColor = generateRandomWhitishColor(
+        preference: randomMainColorPicker,
+        subPreference: randomSubColorPicker,
+        start: 100,
+        prominance: 0.5);
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          counter++;
+        });
+      },
+      child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: MediaQuery.of(context).size.height * 0.6,
+          color: mainColor,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  counter.toString(),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+                _ColorfullButton2(),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class _ColorfullButton2 extends StatefulWidget {
+  const _ColorfullButton2({super.key});
+
+  @override
+  State<_ColorfullButton2> createState() => _ColorfullButton2State();
+}
+
+class _ColorfullButton2State extends State<_ColorfullButton2> {
+  late int counter;
+
+  @override
+  void initState() {
+    counter = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int randomMainColorPicker = math.Random().nextInt(3);
+    int randomSubColorPicker = math.Random().nextInt(3);
+    Color mainColor = generateRandomWhitishColor(
+        preference: randomMainColorPicker,
+        subPreference: randomSubColorPicker,
+        start: 100,
+        prominance: 0.5);
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          counter++;
+        });
+      },
+      child: Container(
+          width: MediaQuery.of(context).size.width * 0.4,
+          height: MediaQuery.of(context).size.height * 0.2,
+          color: mainColor,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  counter.toString(),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
